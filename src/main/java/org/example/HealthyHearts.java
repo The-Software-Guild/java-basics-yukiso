cd7e61045e242cdb2803de2efb0ad8869b478d91
Roland Fazakas <roly.fzks@me.com>
2022-06-14 22:17:25 +0300

    Add initial HealthyHearts

---
+++ src/main/java/org/example/HealthyHearts.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class HealthyHearts {
+    public static void main(String[] args) {
+        int heartRate = 220;
+        System.out.print("What is your age? ");
+        Scanner s = new Scanner(System.in);
+        heartRate -= s.nextInt();
+        System.out.printf("Your maximum heart rate should be %d beats per minute\n" +
+                        "Your target HR Zone is %d - %d beats per minute\n",
+                heartRate, Math.round(heartRate * .5), Math.round(heartRate * .85));
+    }
+}
