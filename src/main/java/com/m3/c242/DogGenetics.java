7bcd91ebf03d3d50c3c17e21ffbc6d932f960163
Roland Fazakas <roly.fzks@me.com>
2022-06-17 17:03:19 +0300

    Improve randomness in DogGenetics

--- src/main/java/com/m3/c242/DogGenetics.java
+++ src/main/java/com/m3/c242/DogGenetics.java
@@ -1,32 +1,27 @@
 package com.m3.c242;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class DogGenetics {
     public static void main(String[] args) {
-        int limit = 101, currentPercentage;
         String dogName;
         Scanner s = new Scanner(System.in);
         Random rand = new Random();
+        int[] rands = rand.ints(5, 0, Short.MAX_VALUE / 5).toArray();
+        int[] dogs = Arrays.stream(rands).map(i -> Math.round(i * 100.0f / Arrays.stream(rands).sum())).toArray();
+        dogs[rand.nextInt(dogs.length)] += 100 - Arrays.stream(dogs).sum();
+
         System.out.print("What is your dog's name? ");
         dogName = s.nextLine();
-        System.out.printf("Well then, I have this highly reliable report on %s's prestigious background right here.\n" +
-                        "\n" +
-                        "%s is:\n" +
-                        "\n" +
-                        "%d%% St. Bernard\n" +
-                        "%d%% Chihuahua\n" +
-                        "%d%% Dramatic RedNosed Asian Pug\n" +
-                        "%d%% Common Cur\n" +
-                        "%d%% King Doberman\n" +
-                        "\n" +
-                        "Wow, that's QUITE the dog!\n",
-                dogName, dogName,
-                currentPercentage = rand.nextInt(limit),
-                currentPercentage = rand.nextInt(limit -= currentPercentage),
-                currentPercentage = rand.nextInt(limit -= currentPercentage),
-                currentPercentage = rand.nextInt(limit -= currentPercentage),
-                limit - currentPercentage - 1);
+        System.out.printf("Well then, I have this highly reliable report on %s's prestigious background right here.\n\n", dogName);
+        System.out.printf("%s is:\n\n", dogName);
+        System.out.println(dogs[0] + "% St. Bernard");
+        System.out.println(dogs[1] + "% Chihuahua");
+        System.out.println(dogs[2] + "% Dramatic RedNosed Asian Pug");
+        System.out.println(dogs[3] + "% Common Cur");
+        System.out.println(dogs[4] + "% King Doberman\n");
+        System.out.println("Wow, that's QUITE the dog!");
     }
 }
